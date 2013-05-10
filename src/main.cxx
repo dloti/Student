2049c274f767f99dcac68fee191142dfc0336aaf
dloti <dloti@aigb-PC.(none)>
2013-05-10 13:36:17 +0200

    first commit

---
+++ src/main.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <fstream>
+#include <typeinfo>
+#include <cmath>
+
+
+int main(int argc, char** argv) {
+//	if (argc < 3) {
+//		std::cerr << "No prob description provided, bailing out!" << std::endl;
+//		std::exit(1);
+//	}
+
+	return 0;
+}
